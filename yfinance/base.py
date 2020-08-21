d2e74ee9adda632135ea62e02f90c829d1309210
Sidhant Chopra <sid.chopra@monash.edu>
2020-08-21 22:51:38 +1000

    Update base.py

--- yfinance/base.py
+++ yfinance/base.py
@@ -280,10 +280,17 @@ class TickerBase():
         data = utils.get_json(url, proxy)
 
         # holders
-       # url = "{}/{}".format(self._scrape_url, self.ticker)
+        url = "{}/{}".format(self._scrape_url, self.ticker)
         holders = _pd.read_html(url+'\holders') 
-        self._major_holders = holders[0]
-        self._institutional_holders = holders[1]
+        
+        f len(holders) > 1:
+             self._institutional_holders = holders[1]
+        else:
+             self._institutional_holders = ""
+        
+        
+       # self._major_holders = holders[0]
+       # self._institutional_holders = holders[1]
 
         # sustainability
         d = {}
